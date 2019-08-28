b0348df313bd47eed217df917fd8bea289a5f701
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-28 14:34:07 +0100

    Added button to the home page

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { Button, ThemeProvider, Header } from "react-native-elements";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button
+} from "react-native";
+import { ThemeProvider, Header } from "react-native-elements";
 import ImageLogo from "./ImageLogo";
+import ProgressCircle from "react-native-progress-circle";
 
 export default class HomePage extends React.Component {
+  state = {
+    address: "1 Portland Street, Manchester, M1 1AA",
+    firstName: "John",
+    surname: "Smith",
+    rent: "500.00",
+    tenancyExpires: "29th September 2019",
+    daysRemaining: 12
+  };
+
   render() {
+    const {
+      daysRemaining,
+      address,
+      firstName,
+      surname,
+      rent,
+      tenancyExpires
+    } = this.state;
+    const fullName = firstName + " " + surname;
+
     return (
       <View style={styles.container}>
         <Header
@@ -20,7 +47,32 @@ export default class HomePage extends React.Component {
           }}
         />
 
-        <View></View>
+        <View style={styles.content}>
+          <Text style={styles.font}>{address}</Text>
+          <Text style={styles.font}>Rent Due</Text>
+          <ProgressCircle
+            style={styles.graph}
+            percent={60}
+            radius={50}
+            borderWidth={8}
+            color="#ED3192"
+            shadowColor="#4A306E"
+            bgColor="#4A306E"
+          >
+            <Text style={styles.font}>{daysRemaining} days</Text>
+          </ProgressCircle>
+          <Text style={styles.font}>{fullName}</Text>
+          <Text style={styles.font}>Rent: £{rent}</Text>
+          <Text style={styles.font}>Tenancy Expires: {tenancyExpires}</Text>
+          <Button
+            title="Maintenance"
+            onPress={() => this.props.navigation.navigate("Maintenance")}
+          />
+          <Button
+            title="Chat"
+            onPress={() => this.props.navigation.navigate("Chat")}
+          />
+        </View>
       </View>
     );
   }
@@ -31,10 +83,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#4A306E",
     alignItems: "center"
   },
-  logoContainer: {
-    alignItems: "center",
+  content: {
+    flex: 1,
+    backgroundColor: "#4A306E",
     justifyContent: "center",
-    flex: 1
+    alignItems: "center"
   },
-  logo: { width: 450, height: 150 }
+  // logoContainer: {
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   flex: 1
+  // },
+  logo: { width: 450, height: 150 },
+  font: {
+    fontSize: 20,
+    color: "#fff"
+  },
+  graph: {
+    paddingTop: 40
+  }
 });
